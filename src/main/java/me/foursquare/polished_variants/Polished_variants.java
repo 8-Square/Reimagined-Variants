c198c8324824041bc283eb559fd62e149d28ac4a
4_Square <104998118+8-Square@users.noreply.github.com>
2025-08-19 10:36:12 +1000

    initial commit

---
+++ src/main/java/me/foursquare/polished_variants/Polished_variants.java
@@ -0,0 +1,10 @@
+package me.foursquare.polished_variants;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Polished_variants implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}

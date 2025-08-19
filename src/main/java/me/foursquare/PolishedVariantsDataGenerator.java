e484f824e85fcd1cf83d603a2c279ace8e39ba89
4_Square <104998118+8-Square@users.noreply.github.com>
2025-08-19 18:37:37 +1000

    changed and revised to use the offical fabric template

---
+++ src/main/java/me/foursquare/PolishedVariantsDataGenerator.java
@@ -0,0 +1,11 @@
+package me.foursquare;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PolishedVariantsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
